4bff32eb280b3a7eb1f53e48f081ba6a540c0543
9pepsi <gex3o.appleid@gmail.com>
2023-06-05 01:01:47 +0300

    added error handling and pm2
    adder error handling
    added pm2 for clustering in prod env
    added api caching
    added logging in prod
    added standard lint rules

--- src/models/Item.js
+++ src/models/Item.js
@@ -1,41 +1,41 @@
-const paginationPlugin = require("@mother/mongoose-cursor-pagination");
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const paginationPlugin = require('@mother/mongoose-cursor-pagination')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 const ItemSchema = new Schema({
-    hash: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    dt: {
-        type: String,
-        required: true
-    },
-    cat: {
-        type: String,
-        required: true
-    },
-    size: {
-        type: Number,
-        required: true
-    },
-    ext_id: {
-        type: String,
-        required: true,
-        unique: true
-    }
-});
+  hash: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  dt: {
+    type: String,
+    required: true
+  },
+  cat: {
+    type: String,
+    required: true
+  },
+  size: {
+    type: Number,
+    required: true
+  },
+  ext_id: {
+    type: String,
+    required: true,
+    unique: true
+  }
+})
 
 // Required for pagination
-ItemSchema.plugin(paginationPlugin);
+ItemSchema.plugin(paginationPlugin)
 ItemSchema.index({ date: -1, _id: -1 })
 
 // Required for full-text search
-ItemSchema.index({ title : 'text', cat : 'text' })
+ItemSchema.index({ title: 'text', cat: 'text' })
 
-module.exports = mongoose.model('Item', ItemSchema);
+module.exports = mongoose.model('Item', ItemSchema)
